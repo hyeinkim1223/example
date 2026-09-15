f64b4ba31db04581f4877e7f4205b3ef754922e0
개발팀 <>
2026-09-15 09:39:04 +0900

    AccountTable 유효성 검사 추가

--- src/components/AccountTable/AccountTable.jsx
+++ src/components/AccountTable/AccountTable.jsx
@@ -4,7 +4,19 @@ import Button from '../Form/Button';
 import Toast from '../Toast';
 import AccountRow from './AccountRow';
 
-export default function AccountTable({ accounts, setAccounts, accountNo }) {
+export default function AccountTable({
+  accounts,
+  setAccounts,
+  accountNo,
+  owner,
+}) {
+  const isAllVaild =
+    accounts.length > 0 && // 배열에 길이가 0보다 크고
+    // every() : 배열 내 하나라도 만족하지 않으면 false
+    accounts.every((account) => {
+      // 계좌번호가 빈칸이 아니면 true, 예금주도 빈칸이 아니면 true
+      return accountNo !== '' && owner !== '';
+    });
   return (
     <>
       <div className={styles.container}>
@@ -34,7 +46,7 @@ export default function AccountTable({ accounts, setAccounts, accountNo }) {
         </div>
         <div className={styles.tableFooter}>
           <div className={styles.totalAccount}>
-            총 5개 계좌 · 모두 유효(every): true
+            총 {accounts.length}개 계좌 · 모두 유효(every): {String(isAllVaild)}
           </div>
           <div className={styles.vipName}>VIP(100만↑): 최지우, 이영희</div>
         </div>
